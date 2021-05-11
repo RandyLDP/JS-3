18ddabb67f7c64fce7655c67702314bab6eab36d
RandyLDP <80972485+RandyLDP@users.noreply.github.com>
2021-05-11 23:07:47 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+const age = 18;
+if (age >= 18){
+    console.log('Mag naar binnen')
+} else{
+    console.log('Mag Niet naar binnen')
+}
+
+const isFemale = true;
+if (isFemale){
+    console.log('Ladys night')
+
+} else{
+    console.log('Not your night')
+}
+
+const driverStatus = 'bob'
+if (driverStatus=='bob'){
+    console.log('Mag rijden')
+} else{
+    console.log('Mag niet rijden')
+}
